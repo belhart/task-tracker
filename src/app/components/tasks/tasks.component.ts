59a4f3835db1738bec5fcf3138bd4db24d79660b
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-12 17:41:12 +0200

    tasks moved to a service

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../../services/task.service";
 import {Task} from "../../Task";
-import {TASKS} from "../../mock-tasks";
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
   }
 
 }
