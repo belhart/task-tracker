1496d9d83e5578fe6cb01cd9cb5ca2758de724e1
Tamás Pap <43545373+belhart@users.noreply.github.com>
2021-06-13 11:36:41 +0200

    router added for home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
@@ -9,6 +11,10 @@ import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const appRoutes: Routes = [
+  {path: '', component: TasksComponent}
+]
 
 @NgModule({
   declarations: [
@@ -23,7 +29,8 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     BrowserModule,
     FontAwesomeModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
